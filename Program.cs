f8ac7160b5edca304ae28310d3b6a835fe4c81f2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:42:45 -0300

    ATIVIDADE CONLCUIDA

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿double primeiro, segundo;
+
+Console.WriteLine("---Atividade Maior2Numero---");
+
+Console.Write("Digite o primeiro numero:");
+primeiro = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo numero:");
+segundo = Convert.ToDouble(Console.ReadLine());
+
+if (primeiro > segundo)
+{
+    Console.WriteLine($"O maior numero entre os dois é: {primeiro}");
+}
+
+else
+{
+    Console.WriteLine($"O maior numero entre os dois é: {segundo}");
+}
+
