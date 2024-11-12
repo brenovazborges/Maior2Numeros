bda36c81285f3aa166ed85a64cceae8adc5f45d9
brenovazborges <brenovaz73@gmail.com>
2024-11-11 23:03:23 -0300

    deixando esse codigo bonitokkk

--- Program.cs
+++ Program.cs
@@ -1,20 +1,12 @@
 ﻿double primeiro, segundo;
-
-Console.WriteLine("---Atividade Maior2Numero---");
-
-Console.Write("Digite o primeiro numero:");
+Console.WriteLine("---Atividade Maior2Numero---\n");
+Console.Write("Digite o primeiro numero: ");
 primeiro = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Digite o segundo numero:");
+Console.Write("Digite o segundo numero: ");
 segundo = Convert.ToDouble(Console.ReadLine());
-
-if (primeiro > segundo)
-{
+Console.WriteLine("");
+if (primeiro > segundo){
     Console.WriteLine($"O maior numero entre os dois é: {primeiro}");
-}
-
-else
-{
+} else {
     Console.WriteLine($"O maior numero entre os dois é: {segundo}");
 }
-
